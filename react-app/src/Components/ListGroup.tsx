e14e030fd8e10902ff68fd7aee724c8b78d1c985
Juan Pablo <juanvalenzuela4business@gmail.com>
2023-11-02 15:49:10 -0600

    Updating the logic functions

--- react-app/src/Components/ListGroup.tsx
+++ react-app/src/Components/ListGroup.tsx
@@ -1,13 +1,21 @@
 function ListGroup() {
   let cities = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
 
+  const handleClick = () => {};
+
   return (
     <>
       <h1>List title</h1>
       {cities.length === 0 && <p>No items found</p>}
       <ul className="list-group">
         {cities.map((item) => (
-          <li key={item}>{item}</li>
+          <li
+            key={item}
+            className="list-group-item"
+            onClick={() => handleClick}
+          >
+            {item}
+          </li>
         ))}
       </ul>
     </>
