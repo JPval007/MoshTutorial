016d6d4e29c98416f9943036b17462cbd7560703
Juan Pablo <juanvalenzuela4business@gmail.com>
2023-11-02 16:06:48 -0600

    operation with handle function

--- react-app/src/Components/ListGroup.tsx
+++ react-app/src/Components/ListGroup.tsx
@@ -1,18 +1,29 @@
+import { MouseEvent } from "react";
+
 function ListGroup() {
   let cities = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  let selectedIndex = 0; //tracks which item is selected in order to highlight it
 
-  const handleClick = () => {};
+  //Event handler
+  //Type annotation to specify the oobject type(only in typescript)
+  const handleClick = (event: MouseEvent) => {
+    console.log(event);
+  };
 
   return (
     <>
       <h1>List title</h1>
       {cities.length === 0 && <p>No items found</p>}
       <ul className="list-group">
-        {cities.map((item) => (
+        {cities.map((item, index) => (
           <li
             key={item}
-            className="list-group-item"
-            onClick={() => handleClick}
+            className={
+              selectedIndex == index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            onClick={handleClick}
           >
             {item}
           </li>
