1d45af45b40d54d29b0dfd60facd6783e74bf6ff
Juan Pablo <juanvalenzuela4business@gmail.com>
2023-11-02 15:06:29 -0600

    http://localhost:5173/ Updating

---
+++ react-app/src/Message.tsx
@@ -0,0 +1,14 @@
+// Tsx is for typescript react components
+
+// Pascal Casing -> Capitalize every letter of each word
+// Always follow this for react components
+function Message() {
+    
+    const name ="Mosh";
+    //JSC Javascript XML
+    return (
+        <h1>Hello World {name}</h1>
+    );
+}
+
+export default Message;
