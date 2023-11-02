20be6fae3f0aafd46097eeda14e0d8dd5d3da3de
Juan Pablo <juanvalenzuela4business@gmail.com>
2023-11-02 16:32:53 -0600

    Beggining with alert and useState and finishing props

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,10 +1,15 @@
 import ListGroup from "./Components/ListGroup";
 
-function App(){
+function App() {
+  let cities = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
 
   return (
     <div>
-      <ListGroup />
+      <ListGroup
+        items={cities}
+        heading="Cities"
+        onSelectedItem={(item) => console.log(item)}
+      />
     </div>
   );
 }
--- react-app/src/Components/ListGroup.tsx
+++ react-app/src/Components/ListGroup.tsx
@@ -1,21 +1,25 @@
-import { MouseEvent } from "react";
+import { useState } from "react";
 
-function ListGroup() {
-  let cities = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
-  let selectedIndex = 0; //tracks which item is selected in order to highlight it
+interface Props {
+  items: string[];
+  heading: string;
+  onSelectedItem: (item: string) => void;
+}
+
+function ListGroup({ items, heading, onSelectedItem }: Props) {
+  //a HOOK is a function that allows us to tap into components
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+  //   const [name, setName] = useState('');
 
   //Event handler
   //Type annotation to specify the oobject type(only in typescript)
-  const handleClick = (event: MouseEvent) => {
-    console.log(event);
-  };
 
   return (
     <>
-      <h1>List title</h1>
-      {cities.length === 0 && <p>No items found</p>}
+      <h1>{heading}</h1>
+      {items.length === 0 && <p>No items found</p>}
       <ul className="list-group">
-        {cities.map((item, index) => (
+        {items.map((item, index) => (
           <li
             key={item}
             className={
@@ -23,7 +27,10 @@ function ListGroup() {
                 ? "list-group-item active"
                 : "list-group-item"
             }
-            onClick={handleClick}
+            onClick={() => {
+              setSelectedIndex(index);
+              onSelectedItem(item);
+            }}
           >
             {item}
           </li>
